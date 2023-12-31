7248436ef77df1acd1d1915546c952bca59ab44d
SiKarmen <82767449+SiKarmen@users.noreply.github.com>
2023-12-31 23:52:41 +0530

    Add files via upload

--- AtCoder_ProfileAnalyzer.py
+++ AtCoder_ProfileAnalyzer.py
@@ -1,1 +1,8 @@
-print('Hello World')
+from bs4 import BeautifulSoup
+import requests
+
+
+name = input('Input your AtCoder username: ')
+response = requests.get(f'https://atcoder.jp/users/{name}').text
+
+print(response)
